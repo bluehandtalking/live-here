ad2bf93bb047f307b0cc578fa1a22ed399e237b5
amanda spiegel <amandakspiegel@gmail.com>
2014-02-04 14:15:27 -0800

    clearing changes so merge can happen

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ LiveHere::Application.routes.draw do
 
   resources :quizzes
 
-  root to: 'welcome#index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
