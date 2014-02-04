8b6e2ca0ae83f00459d03fed865b8215b5ffaff3
Jerrold Thompson <jet@whidbey.com>
2014-02-04 10:54:56 -0800

    Testing environment

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ LiveHere::Application.routes.draw do
 
   resources :quizzes
 
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'wo:elcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ test/features/users/visit_site_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+feature "Mover who wishes to make intelligent decision regarding their move visits our site" do
+  scenario "Mover visits entry portal" do
+    visit root_path
+  end
+  scenario "Mover clicks link to start the moving decision quiz" do
+    click_link "Start the Quiz"
+  end
+end
+
+# class Users::VisitSiteTest < Capybara::Rails::TestCase
+#   test "sanity" do
+#     visit root_path
+#     assert_content page, "Hello World"
+#     refute_content page, "Goobye All!"
+#   end
+# end
