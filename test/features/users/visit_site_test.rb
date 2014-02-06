dd0a6be9bf5b548406df29cce4db6686c56467bb
amanda spiegel <amandakspiegel@gmail.com>
2014-02-06 11:54:14 -0800

    fixing user fixtures error

--- test/features/users/visit_site_test.rb
+++ test/features/users/visit_site_test.rb
@@ -3,9 +3,49 @@ require "test_helper"
 feature "Mover who wishes to make intelligent decision regarding their move visits our site" do
   scenario "Mover visits entry portal" do
     visit root_path
+    page.must_include "Contact Us"
   end
   scenario "Mover clicks link to start the moving decision quiz" do
     visit root_path
     click_link "Start the Quiz"
   end
+  #scenario "Mover chooses an answer for each question" do
+   # visit root_path
+    #click_link "Start the Quiz"
+    #topic.each select "A"
+    #click_button "Tell Me Where to Move!"
+    #page.must_include "Type A"
+  #end
 end
+
+#feature "Mover wants to know more about us" do
+  #scenario "mover visits about us page" do
+    #visit root_path
+    #click "About Us"
+    #page.must_include "Who We Are"
+  #end
+#end
+
+feature "Mover wants to have an account with us" do
+  scenario "mover signs up" do
+    visit root_path
+    click_link "Sign Up"
+    page.must_include "Password Confirmation"
+  end
+  scenario "mover signs in" do
+   sign_in(:user)
+   page.must_include "Signed"
+ end
+end
+
+
+
+#feature "Mover wants to browse cities" do
+ # scenario "mover visits browse cities page" do
+  #  visit root_path
+   # click_link "Browse Cities"
+    #page.must_include "Seattle"
+  #end
+end
+
+
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,6 +25,14 @@ class FeatureTest < MiniTest::Spec
   register_spec_type(/integration$/, self)
 end
 
+def sign_in
+  visit root_path
+  click_link "Sign In"
+  page.must_include "Remember me"
+  fill_in "Email", with: users(:user).email
+  fill_in "Password", with: users(:user).password
+  click_button "Sign In"
+end
 Turn.config.format = :outline
 
 
